40e32aca134d62c362aab28d64734d93b10704e6
Alex-4lex <alex20014lex@gmail.com>
2024-11-03 02:03:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let data = [
         'id': 'a1',
         'title': 'جيش بشار الأسد يقومون بضرب وتعذيب مدني اعزل، بتاريخ: 11/7/2012 ',
         'name': 'manipulate text background.mp4',
-        'duration': '0:22',
+        'duration': '2:57',
     },
     {
         'id': 'a2',
